554dbcb68f31d2338100859ba8633aa0da1701c1
York Rushin <rushinyork@gmail.com>
2022-06-09 20:16:35 -0700

    commit tree 8989728a1256713c0e9b8946858ef3827f0a58a7

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+let blockDist
+let locHQ = 42
+let feetDist
+let travelDist
+let travelPrice
+function distanceFromHqInBlocks(someBlocks) {
+    blockDist = someBlocks - locHQ;
+    return Math.abs(blockDist)
+}
+
+function distanceFromHqInFeet(someFeet) {
+    blockDist = someFeet - locHQ;
+    feetDist = blockDist * 264;
+    return Math.abs(feetDist)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    travelDist = (destination - start) * 264;
+    return Math.abs(travelDist)
+}
+
+function calculatesFarePrice(start, destination) {
+    travelDist = Math.abs(destination - start) * 264;
+    if (travelDist < 400) {
+        travelPrice = 0;
+    } else if (travelDist < 2000) {
+        travelDist = travelDist - 400;
+        travelPrice = travelDist * 0.02;
+    } else if (travelDist < 2500) {
+        travelPrice = 25;
+    } else if (travelDist > 2500) {
+        travelPrice = 'cannot travel that far'
+    }
+    return travelPrice
+}
